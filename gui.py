d03d9485b9daf163ade6d32c7c7a56859b6e4f46
sachdevabhavya <sach.bhavya@gmail.com>
2024-07-18 15:52:30 +0530

    gui

--- gui.py
+++ gui.py
@@ -1,14 +1,21 @@
 from tkinter import *
-from weather import send_temperature_graph_email
+from weather import send_temperature_graph_email , get_weather
 
 def on_send_email():
     to_email = e1.get()
-    if to_email:
+    city = e2.get()
+    if not city:
+        print("Please Enter city")
+    
+    if to_email and city:
         try:
-            send_temperature_graph_email(to_email)
+            send_temperature_graph_email(to_email , city)
+            get_weather(city)
             status_label.config(text="Email sent successfully.", fg="green")
+            
         except Exception as e:
             status_label.config(text=f"Error sending email: {str(e)}", fg="red")
+
     else:
         print("Please enter an email address.")
 
@@ -24,6 +31,10 @@ frame.pack(pady=10)
 Label(frame, text="Enter email").pack(side=LEFT)
 e1 = Entry(frame)
 e1.pack(side=LEFT, padx=5)
+
+Label(frame, text="Enter city").pack(side=LEFT)
+e2 = Entry(frame)
+e2.pack(side=LEFT, padx=5)
 
 button = Button(root, text='Send email', width=50, command=on_send_email)
 button.pack(pady=20)
